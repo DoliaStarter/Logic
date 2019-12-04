abed0bc5820a7241e48929fcb49b8f22d48ad18b
Bohdan Forostianyi <qwebeck5@gmail.com>
2019-12-04 11:39:45 +0100

    Delete redundant predicate in include_assoc.[l

--- metaprogramming/include_assoc.pl
+++ metaprogramming/include_assoc.pl
@@ -1,6 +1,5 @@
 klucz_mniejszy_od_wartosci(K-V) :- K < V.
-ifelse(A,Succ,_):-call(A),!,call(Succ).
-ifelse(_,_,Fl):-call(Fl).
+
 include_assoc(_,Dict,Result):-
     		empty_assoc(Dict),
     		list_to_assoc([],Result).
@@ -9,4 +8,5 @@ include_assoc(Action,Dict,Result):-
     		Condition =.. [Action,Key-Value],
     		del_min_assoc(Dict,Key,Value,ErasedDict),
     		include_assoc(Action,ErasedDict,MidResult),
-    		ifelse(Condition,put_assoc(Key,MidResult,Value,Result),true).
+    		(Condition -> put_assoc(Key,MidResult,Value,Result);true).
+
